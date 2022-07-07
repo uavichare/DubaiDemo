2dc68a19751c07820bbd14ff688b5e3fe719c6ac
dilipgupta123-123 <GDIlip10#$%>
2022-07-07 16:31:16 +0530

    add new method

--- dubaiCultureLibrary/src/main/java/com/example/dubaiculturelibrary/TestActivity.java
+++ dubaiCultureLibrary/src/main/java/com/example/dubaiculturelibrary/TestActivity.java
@@ -28,7 +28,7 @@ public class TestActivity extends AppCompatActivity implements MyListener {
 
     public void callToast()
     {
-        Toast.makeText(this, "uday bevda", Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(this, "uday bevda", Toast.LENGTH_SHORT).show();
 /*
         MyButton button=new MyButton(this);
         button.MyLogicToIntimateOthers("uday bevda");
