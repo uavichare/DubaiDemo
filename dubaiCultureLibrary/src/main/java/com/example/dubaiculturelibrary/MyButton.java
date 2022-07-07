9d319b1a5eb6af262d208391c503ec138264d00b
dilipgupta123-123 <GDIlip10#$%>
2022-07-07 13:15:59 +0530

    added new call

--- dubaiCultureLibrary/src/main/java/com/example/dubaiculturelibrary/MyButton.java
+++ dubaiCultureLibrary/src/main/java/com/example/dubaiculturelibrary/MyButton.java
@@ -6,10 +6,12 @@ public class MyButton {
     MyListener ml;
 
     // constructor
+/*
     public MyButton(MyListener ml) {
         //Setting the listener
         this.ml = ml;
     }
+*/
 
     public void MyLogicToIntimateOthers(Context c) {
         //Invoke the interface
