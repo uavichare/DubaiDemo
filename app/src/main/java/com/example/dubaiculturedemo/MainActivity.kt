fe2fb91ca834d27232eb7b6d014ee3f3c05a8c0f
dilipgupta123-123 <GDIlip10#$%>
2022-07-10 22:33:51 +0530

    added mybutton in proguard rules

--- app/src/main/java/com/example/dubaiculturedemo/MainActivity.kt
+++ app/src/main/java/com/example/dubaiculturedemo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.dubaiculturedemo
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dubaiculturelibrary.MyListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MyListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun s(message: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNavFragment(c: Context?) {
+        TODO("Not yet implemented")
     }
 }
